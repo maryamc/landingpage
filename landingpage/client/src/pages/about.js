e1b5bce11d78a4ecbd9f75a912b219c4d12c5db7
Maryam Chaudhry <maryamc1798@gmail.com>
2021-11-05 14:00:09 -0400

    trying to get components to show up

---
+++ landingpage/client/src/pages/about.js
@@ -0,0 +1,13 @@
+import React, {useEffect} from 'react';
+import AboutComponent from "../components/about/AboutComponent";
+
+function about() {
+
+    return (
+        <div>
+            <AboutComponent />
+        </div>
+    )
+}
+
+export default about
--- landingpage/client/src/pages/home.js
+++ landingpage/client/src/pages/home.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import HomeComponent from "../components/home/HomeComponent";
 
-function Home() {
+function home() {
 
     return (
         <div>
@@ -10,4 +10,4 @@ function Home() {
     )
 }
 
-export default Home
+export default home
