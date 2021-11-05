5fdacd8ba6e94293b4d75b6ca3ed57babebac29d
Maryam Chaudhry <maryamc1798@gmail.com>
2021-11-05 11:43:28 -0400

    arranging file structure

---
+++ landingpage/server.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+// const mongoose = require("mongoose");
+
+//requiring ports
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require("./routes");
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Connect to the Mongo DB
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/user", 
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true, 
+//     useCreateIndex: true, 
+//     useFindAndModify: false
+//   });
+
+// Use apiRoutes
+app.use(apiRoutes);
+
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
