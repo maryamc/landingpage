febf04a115221adb6c900785112f6ecf357b425a
Maryam Chaudhry <maryamc1798@gmail.com>
2021-11-05 13:12:10 -0400

    adjusting routes and creating basic home page test

---
+++ landingpage/client/src/components/home/HomeComponent.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./home.css";
+
+function HomeComponent() {
+    return (
+        <div>
+            <div id="homeDiv">
+                <div className="welcomeSection">
+                    <div className="welcomeLeft">
+                        <h1>
+                            Excelsior Diagnostic Laboratories
+                        </h1>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default HomeComponent
---
+++ landingpage/client/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomeComponent from "../components/home/HomeComponent";
+
+function Home() {
+
+    return (
+        <div>
+            <HomeComponent />
+        </div>
+    )
+}
+
+export default Home
