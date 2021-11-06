d1343ba01215b18708eb1eed70bae3bcfa3bf865
Maryam Chaudhry <maryamc1798@gmail.com>
2021-11-05 20:35:06 -0400

    working on location

--- landingpage/client/src/components/logo/logo.js
+++ landingpage/client/src/components/logo/logo.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./logo.css"
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import PhoneIcon from '@mui/icons-material/Phone';
 
 
 function Logo() {
     return(
-        <div className="container">
-            <div className="logoContainer">
+        <div>
+            {/* <div className="logoContainer"> */}
                 <ul className="nav nav-tabs" id="logoNav">
-                 <li className="nav-item">
+                 <li className="nav-item logo">
                      <p> <LocationOnIcon/> 2720 NJ-27 North Brunswick, NJ 08902</p>
+                 </li>
+                 <li className="nav-item logo" id="Phone"> 
+                 <p>
+                     <PhoneIcon/> 732-422-7358
+                 </p>
                  </li>
                 </ul>
                 
-            </div>
+            {/* </div> */}
 
         </div>
     )
