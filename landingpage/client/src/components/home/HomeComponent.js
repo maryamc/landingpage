0fadfc888fc9d7cc1ece43121ada9c62871a52ad
Maryam Chaudhry <maryamc1798@gmail.com>
2021-11-12 14:31:12 -0500

    working on homepage carousel

--- landingpage/client/src/components/home/HomeComponent.js
+++ landingpage/client/src/components/home/HomeComponent.js
@@ -1,19 +1,61 @@
 import React from "react";
 import "./home.css";
-// import images from "../../images.json"
+import Pipette from "../../pipette.png";
+import Blood from "../../bloodpath.jpeg";
+import Covid from "../../covid2.jpeg";
+import { Carousel } from 'react-bootstrap'
 
 function HomeComponent() {
     return (
         <div>
             <div className="container">
-                <p className="hello">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <Carousel fade>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 h-50"
+                            src={Covid}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 h-50"
+                            src={Blood}
+                            alt="Second slide"
+                        />
 
-                </p>
-    
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 h-50"
+                            src={Pipette}
+                            alt="Third slide"
+                        />
 
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+
+                <div className="container second">
+                    <p className="hello">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                    </p>
+
+
+                </div>
             </div>
-
         </div>
     )
 }
